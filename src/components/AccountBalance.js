02d4f1407c96ffe962869128eff310665ec67fbf
S <abc123@gmail.com>
2023-04-20 19:50:55 -0400

    Attempted to fix Account balance

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -8,6 +8,16 @@ import React, {Component} from 'react';
 class AccountBalance extends Component {
   // Display account balance
   render() {
+    const creditTotal = this.props.creditList.reduce(
+      (total, credit) => total + credit.amount,
+      0
+    );
+    const debitTotal = this.props.debitList.reduce(
+      (total, debit) => total + debit.amount,
+      0
+    );
+    const accountBalance = creditTotal - debitTotal;
+
     return (
       <div>
         Balance: {this.props.accountBalance}
