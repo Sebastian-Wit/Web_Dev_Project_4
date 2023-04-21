cacbedf10c92149b48fc27042a8f02d9f7aed4b7
S <abc123@gmail.com>
2023-04-20 21:42:20 -0400

    Adding Credit History

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -4,39 +4,33 @@ src/components/Credits.js
 The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
-import { Link } from 'react-router-dom';
-import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
-class Credits extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      creditList: [],
-    };
+const Credits = (props) => {
+  // Create the list of Debit items
+  let CreditsView = () => {
+    const { credits } = props;
+    return credits.map((credit) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
+      let date = credit.date.slice(0,10);
+      return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
+    });
   }
+  // Render the list of Credit items and a form to input new Credit item
+  return (
+    <div>
+      <h1>Credits</h1>
 
-  componentDidMount() {
-    fetch('https://johnnylaicode.github.io/api/credits.json')
-      .then((response) => response.json())
-      .then((data) => this.setState({ creditList: data }));
-  }
+      {CreditsView()}
 
-  render() {
-    const credits = this.state.creditList.map((credit) => (
-      <li key={credit.id}>
-        {credit.description} - ${credit.amount.toFixed(2)} - {credit.date}
-      </li>
-    ));
-
-    return (
-      <div>
-        <h1>Credits</h1>
-        <ul>{credits}</ul>
-        <br />
-        <Link to="/">Return to Home</Link>
-      </div>
-    );
-  }
+      <form onSubmit={props.addCredit}>
+        <input type="text" name="description" />
+        <input type="number" name="amount" />
+        <button type="submit">Add Credit</button>
+      </form>
+      <br/>
+      <Link to="/">Return to Home</Link>
+    </div>
+  );
 }
 
 export default Credits;
