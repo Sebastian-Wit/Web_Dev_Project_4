34e8bb6f8a284bf341c9d46916b6d990d24818e7
S <abc123@gmail.com>
2023-04-20 22:19:41 -0400

    Updated Account Balance

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -3,26 +3,17 @@ src/components/AccountBalance.js
 
 The AccountBalance component displays account balance. It is included in other page views.
 ==================================================*/
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class AccountBalance extends Component {
-  //Calculate Account Balance with Credit and Debit array values
-  calculateBalance() {
-    let balance = 1234567.89;
-    for (let i = 0; i < this.props.creditList.length; i++){
-      balance += this.props.creditList[i].amount;
-    }
-    for (let i = 0; i < this.props.debitList.length; i++){
-      balance -= this.props.debitList[i].amount;
-    }
-    return balance;
-  }
-
   // Display account balance
   render() {
+    const { creditList, debitList, accountBalance } = this.props;
+    const balance = creditList.reduce((total, credit) => total + credit.amount, accountBalance) 
+                  - debitList.reduce((total, debit) => total + debit.amount, 0);
     return (
       <div>
-        Balance: {this.calculateBalance()}
+        Balance: {balance.toFixed(2)}
       </div>
     );
   }
