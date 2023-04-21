e2952029308371914a07454baa608105fac18e9a
S <abc123@gmail.com>
2023-04-21 00:13:02 -0400

    Updated Account Balance

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -3,7 +3,7 @@ src/components/AccountBalance.js
 
 The AccountBalance component displays account balance. It is included in other page views.
 ==================================================*/
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class AccountBalance extends Component {
   // Display account balance
