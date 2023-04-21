7ab67a70dce42c797c4aba27d4a3b4c2556dd728
S <abc123@gmail.com>
2023-04-20 21:23:41 -0400

    Attempt to fix code

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ src/App.js
 This is the top-level component of the app.
 It contains the top-level state.
 ==================================================*/
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // Import other components
 import Home from './components/Home';
@@ -15,8 +15,8 @@ import Credits from './components/Credits';
 import Debits from './components/Debits';
 
 class App extends Component {
-  constructor() {  // Create and initialize state
-    super(); 
+  constructor() {
+    super();
     this.state = {
       accountBalance: 1234567.89,
       creditList: [],
@@ -24,31 +24,68 @@ class App extends Component {
       currentUser: {
         userName: 'Joe Smith',
         memberSince: '11/22/99',
-      }
+      },
     };
   }
 
   // Update state's currentUser (userName) after "Log In" button is clicked
-  mockLogIn = (logInInfo) => {  
-    const newUser = {...this.state.currentUser};
+  mockLogIn = (logInInfo) => {
+    const newUser = { ...this.state.currentUser };
     newUser.userName = logInInfo.userName;
-    this.setState({currentUser: newUser})
+    this.setState({ currentUser: newUser });
+  };
+
+  // Fetch credit and debit data from APIs and update state
+  componentDidMount() {
+    fetch('https://johnnylaicode.github.io/api/credits.json')
+      .then((response) => response.json())
+      .then((data) => this.setState({ creditList: data }));
+
+    fetch('https://johnnylaicode.github.io/api/debits.json')
+      .then((response) => response.json())
+      .then((data) => this.setState({ debitList: data }));
   }
 
-  // Create Routes and React elements to be rendered using React components
-  render() {  
+  // Add a new credit to the creditList array in state
+  addCredit = (newCredit) => {
+    this.setState((prevState) => ({
+      creditList: [...prevState.creditList, newCredit],
+      accountBalance: prevState.accountBalance + newCredit.amount,
+    }));
+  };
+
+  // Add a new debit to the debitList array in state
+  addDebit = (newDebit) => {
+    this.setState((prevState) => ({
+      debitList: [...prevState.debitList, newDebit],
+      accountBalance: prevState.accountBalance - newDebit.amount,
+    }));
+  };
+
+  render() {
     // Create React elements and pass input props to components
-    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance} />)
+    const HomeComponent = () => (
+      <Home accountBalance={this.state.accountBalance} />
+    );
     const UserProfileComponent = () => (
-      <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
-    )
-    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
-    const CreditsComponent = () => (<Credits credits={this.state.creditList} />) 
-    const DebitsComponent = () => (<Debits debits={this.state.debitList} />) 
+      <UserProfile
+        userName={this.state.currentUser.userName}
+        memberSince={this.state.currentUser.memberSince}
+      />
+    );
+    const LogInComponent = () => (
+      <LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />
+    );
+    const CreditsComponent = () => (
+      <Credits credits={this.state.creditList} addCredit={this.addCredit} />
+    );
+    const DebitsComponent = () => (
+      <Debits debits={this.state.debitList} addDebit={this.addDebit} />
+    ); 
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
-      <Router basename="/bank-of-react-example-code-gh-pages">
+      <Router basename="/my-react-app">
         <div>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
