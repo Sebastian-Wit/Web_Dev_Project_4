bedf976570bf051196fcef836553ff4e8cf70c56
S <abc123@gmail.com>
2023-04-20 22:04:43 -0400

    Updated Account Balance

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -7,8 +7,8 @@ Note: You need to work on this file for the Assignment.
 import {Link} from 'react-router-dom';
 
 const Credits = (props) => {
-  // Create the list of Debit items
-  let CreditsView = () => {
+  // Create the list of Credit items
+  let creditsView = () => {
     const { credits } = props;
     return credits.map((credit) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
       let date = credit.date.slice(0,10);
@@ -20,7 +20,7 @@ const Credits = (props) => {
     <div>
       <h1>Credits</h1>
 
-      {CreditsView()}
+      {creditsView()}
 
       <form onSubmit={props.addCredit}>
         <input type="text" name="description" />
