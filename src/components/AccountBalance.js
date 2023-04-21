2938f6547a038c2b226eabec725d907b1045420a
S <abc123@gmail.com>
2023-04-20 22:16:13 -0400

    Updated Account Balance

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -8,10 +8,14 @@ import React, {Component} from 'react';
 class AccountBalance extends Component {
   //Calculate Account Balance with Credit and Debit array values
   calculateBalance() {
-    const { creditList, debitList } = this.props;
-    const creditTotal = creditList.reduce((acc, credit) => acc + credit.amount, 0);
-    const debitTotal = debitList.reduce((acc, debit) => acc + debit.amount, 0);
-    return creditTotal - debitTotal;
+    let balance = 1234567.89;
+    for (let i = 0; i < this.props.creditList.length; i++){
+      balance += this.props.creditList[i].amount;
+    }
+    for (let i = 0; i < this.props.debitList.length; i++){
+      balance -= this.props.debitList[i].amount;
+    }
+    return balance;
   }
 
   // Display account balance
