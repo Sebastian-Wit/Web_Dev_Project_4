6a8fc584ea2b6791d202a39d6f62e476c48f0560
S <abc123@gmail.com>
2023-04-20 20:19:46 -0400

    test

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -1,6 +1,5 @@
 /*==================================================
 src/components/AccountBalance.js
-
 The AccountBalance component displays account balance. It is included in other page views.
 ==================================================*/
 import React, {Component} from 'react';
@@ -8,16 +7,6 @@ import React, {Component} from 'react';
 class AccountBalance extends Component {
   // Display account balance
   render() {
-    const creditTotal = this.props.creditList.reduce(
-      (total, credit) => total + credit.amount,
-      0
-    );
-    const debitTotal = this.props.debitList.reduce(
-      (total, debit) => total + debit.amount,
-      0
-    );
-    const accountBalance = creditTotal - debitTotal;
-
     return (
       <div>
         Balance: {this.props.accountBalance}
