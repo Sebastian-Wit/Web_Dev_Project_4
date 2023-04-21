28911b19862c86cc576567e7016cc209ca626216
S <abc123@gmail.com>
2023-04-20 21:39:37 -0400

    Adding Credit History

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -4,36 +4,39 @@ src/components/Credits.js
 The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 
-const Credits = (props) => {
-  const creditList = props.credits.map((credit) => {
-    const date = new Date(credit.date);
-    const formattedDate = `${date.getFullYear()}-${
-      date.getMonth() + 1
-    }-${date.getDate()}`;
-    const roundedAmount = credit.amount.toFixed(2);
+class Credits extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      creditList: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://johnnylaicode.github.io/api/credits.json')
+      .then((response) => response.json())
+      .then((data) => this.setState({ creditList: data }));
+  }
+
+  render() {
+    const credits = this.state.creditList.map((credit) => (
+      <li key={credit.id}>
+        {credit.description} - ${credit.amount.toFixed(2)} - {credit.date}
+      </li>
+    ));
 
     return (
-      <li key={credit.id}>
-        <p>Description: {credit.description}</p>
-        <p>Amount: ${roundedAmount}</p>
-        <p>Date: {formattedDate}</p>
-      </li>
+      <div>
+        <h1>Credits</h1>
+        <ul>{credits}</ul>
+        <br />
+        <Link to="/">Return to Home</Link>
+      </div>
     );
-  });
-
-  return (
-    <div>
-      <h1>Credits</h1>
-      <br />
-      <Link to="/">Return to Home</Link>
-      <br />
-      <br />
-      <h2>Credit List</h2>
-      <ul>{creditList}</ul>
-    </div>
-  );
-};
+  }
+}
 
 export default Credits;
